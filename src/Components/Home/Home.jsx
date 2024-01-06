7a4b20a234f5aeb6e77c6ec79a997f11759ddd93
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2024-01-06 22:27:46 +0530

    removed unused and error causing some import

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 import Container from "../Container/Container";
 import { RandomUserList } from "../../appwrite/config";
 import UserCard from "./UserCard";
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -6,9 +6,8 @@ import { useLoaderData, useParams } from "react-router-dom";
 import { getUserById, updateTheSeenBy } from "../../appwrite/config";
 import ClipLoader from "react-spinners/ClipLoader";
 import { useSelector } from "react-redux";
-import { WhatsappIcon, WhatsappShareButton } from "react-share";
 import { RWebShare } from "react-web-share";
-import { ExternalLink, Share, Share2, Share2Icon, ShieldAlertIcon } from "lucide-react";
+import { Share2 } from "lucide-react";
 export const userCardLoader = async ({ params }) => {
   const res = await getUserById(params.userId);
   return res;
@@ -26,19 +25,17 @@ function User() {
   return (
     <div className="shadow-xl shadow-black  md:w-/6  mx-auto px-auto w-[90%] md:min-h-[78vh]  md:px-1 my-6 mb-8 py-5  rounded-lg bg-purple-700/90 md:rounded-3xl flex justify-center items-start  flex-col relative md:flex-row">
       <div className="self-end absolute top-5 right-5">
-            <RWebShare
-                data={{
-                    text: `Take a look at the profile of ${user.fullName}!`,
-                    url: `\ncrushers.vercel.app/user/${user.$id}`,
-                    title: "Share profile with friends",
-                }}
-                onClick={() =>
-                    console.log("shared successfully!")
-                }
-            >
+        <RWebShare
+          data={{
+            text: `Take a look at the profile of ${user.fullName}!`,
+            url: `\ncrushers.vercel.app/user/${user.$id}`,
+            title: "Share profile with friends",
+          }}
+          onClick={() => console.log("shared successfully!")}
+        >
           <Share2 className="w-10 h-7 rounded-md shadow-lg bg-indigo-700 text-gray-300 p-1 active:text-white active:bg-black font-semibold" />
-            </RWebShare>
-        </div>
+        </RWebShare>
+      </div>
       <div
         className="md:shadow-lg lg:min-w-80 md:w-72 md:h-auto lg:min-h-80 w-28 h-28  mx-auto  md:my-auto my-5 md:p-10 lg:ml-20 md:ml-10 md:bg-gray-300 md:rounded-xl  md:shadow-black
         flex justify-center items-center"
@@ -54,7 +51,7 @@ function User() {
           <ClipLoader loading={loading} size={70} />
         </div>
       </div>
-      
+
       <div className="w-full md:w-2/3 my-auto">
         <Para text={"Name:"} output={user.fullName} />
         <Para text={"Branch:"} output={user.branch} />
