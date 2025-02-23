a288337b8a2c21b5b42a009a2589956ffd97e5d6
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-02-23 22:26:55 +0530

    .

--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Notification() {
     return (
-        <Link to={"/search-search"}>
+        <Link to={"/search"}>
             <div className="py-1 bg-gradient-to-l from-indigo-700 via-purple-700 to-pink-700  text-white cursor-pointer  font-semibold  text-center">
                 Advanced Search Option 🔎🔎
             </div>
