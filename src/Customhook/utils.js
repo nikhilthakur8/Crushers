ba20dc8646c34b7e8677c4d41b385c5169b86a8a
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-02-23 22:23:22 +0530

    .

--- src/Customhook/utils.js
+++ src/Customhook/utils.js
@@ -4,8 +4,8 @@ export const googleUserLogin = async () => {
     try {
         account.createOAuth2Session(
             "google",
-            // "https://crushers.vercel.app/login-with-google",
-            "http://192.168.1.37/login-with-google",
+            "https://crushers.vercel.app/login-with-google",
+            // "http://192.168.1.37/login-with-google",
             "https://crushers.vercel.app/login"
         );
     } catch (error) {
