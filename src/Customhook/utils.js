40c6bfa7c255a4c385d18d3950e5e4fe13d9f9a0
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-09-20 05:51:18 +0530

    Update OAuth2 token URL for Google login and ensure Analytics component is rendered correctly

--- src/Customhook/utils.js
+++ src/Customhook/utils.js
@@ -1,10 +1,10 @@
 import { account } from "../appwrite/auth";
 
 export const googleUserLogin = async () => {
-    account.createOAuth2Token(
-        "google",
-        "https://crushers.vercel.app/login-with-google",
-        // "http://192.168.1.45/login-with-google",
-        "https://crushers.vercel.app/login"
-    );
+	account.createOAuth2Token(
+		"google",
+		"https://crushers.clouly.in/login-with-google",
+		// "http://192.168.1.45/login-with-google",
+		"https://crushers.clouly.in/login"
+	);
 };
